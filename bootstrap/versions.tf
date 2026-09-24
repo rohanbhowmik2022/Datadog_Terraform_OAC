50f4a6c59e37eb5c1429c4413a2e7d103c202aae
Rohan Bhowmik <rohanbhowmik001@gmail.com>
2026-09-25 00:48:47 +0530

    Modified Bootstrap

--- bootstrap/versions.tf
+++ bootstrap/versions.tf
@@ -1,9 +1,18 @@
 terraform {
   required_version = ">= 1.6.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket       = "datadog-terraform-oac-867041163196"
+    key          = "bootstrap/terraform.tfstate"
+    region       = "ap-south-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
